4095af910f403becd190689f85c7132d81443550
Glitch (among-us-friends) <none>
2020-11-09 18:36:47 +0000

    🚉👮 Refactor loading text

--- public/player.js
+++ public/player.js
@@ -4,21 +4,18 @@ const playerList = document.querySelector(".player");
 fetch("/stats")
   .then(response => response.json())
   .then(json => {
-    console.log(json);
     // Remove loading text
-    playerList.firstElementChild.remove();
+    document.querySelector(".loading").remove();
 
     const URLparts = document.location.href.split("/");
     const name = URLparts.pop() || URLparts.pop(); // handle potential trailing slash
     const player = json.players.filter(p => p.name === name)[0];
+    playerTitle.innerHTML += ` - ${name} (<code>${player.elo}</code>)`;
 
     document.querySelector("canvas").id = name;
     window.generatePlayerGraph(player);
-    playerTitle.innerHTML += ` - ${name} (<code>${player.elo}</code>)`;
 
     const gamesList = document.createElement("div");
-    //
-    //
     player.games.forEach(game => {
       const gameElem = document.createElement("p");
       gameElem.classList.add("game-history");
