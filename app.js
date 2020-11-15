3226c7c3e6e9dd32b6ed958d8f0196a693728e92
Andrew <healeycodes@gmail.com>
2020-11-15 16:35:24 +0000

    Lint files 😅

--- app.js
+++ app.js
@@ -1,49 +1,49 @@
-const fs = require("fs");
-const fetch = require("node-fetch");
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-const { buildStats } = require("./stats");
+const fs = require("fs")
+const fetch = require("node-fetch")
+const express = require("express")
+const app = express()
+app.use(express.static("public"))
+const { buildStats } = require("./stats")
 
-const LOG_FILE = "log.txt";
+const LOG_FILE = "log.txt"
 
 app.get("/", (request, response) => {
-  log(request, "home");
-  response.sendFile(__dirname + "/views/index.html");
-});
+    log(request, "home")
+    response.sendFile(__dirname + "/views/index.html")
+})
 
 app.get("/player/:player", (request, response) => {
-  const { player } = request.params;
-  log(request, `player/${player}`);
-  response.sendFile(__dirname + "/views/player.html");
-});
+    const { player } = request.params
+    log(request, `player/${player}`)
+    response.sendFile(__dirname + "/views/player.html")
+})
 
 app.get("/stats", async (request, response) => {
-  const data = await sheetData();
-  const stats = buildStats(data);
-  response.json(stats);
-});
+    const data = await sheetData()
+    const stats = buildStats(data)
+    response.json(stats)
+})
 
 app.get("/raw-stats", async (request, response) => {
-  // Make the league's raw data open for all!
-  const data = await sheetData();
-  response.json(data);
-});
+    // Make the league's raw data open for all!
+    const data = await sheetData()
+    response.json(data)
+})
 
 // Our datasource is a series of rows
 async function sheetData() {
-  return fetch(
-    `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SHEETS_ID}/values/C4:O1000?key=${process.env.SHEETS_API_KEY}`
-  ).then((res) => res.json());
+    return fetch(
+        `https://sheets.googleapis.com/v4/spreadsheets/${process.env.SHEETS_ID}/values/C4:O1000?key=${process.env.SHEETS_API_KEY}`
+    ).then(res => res.json())
 }
 
 function log(request, notes = "") {
-  const msg = `${new Date().toLocaleString()} - ${notes} - ${
-    request.headers["x-forwarded-for"]
-  }\n`;
-  fs.appendFile(LOG_FILE, msg, function (err) {
-    if (err) throw err;
-  });
+    const msg = `${new Date().toLocaleString()} - ${notes} - ${
+        request.headers["x-forwarded-for"]
+    }\n`
+    fs.appendFile(LOG_FILE, msg, function (err) {
+        if (err) throw err
+    })
 }
 
-module.exports = app;
+module.exports = app
