7ebecdc13afc7c196524c8a70451dcf58a5aaa50
Andrew <healeycodes@gmail.com>
2020-12-05 14:10:54 +0000

    Fix failing test due to new stats path

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,34 +1,34 @@
-jest.mock("node-fetch");
-const fetch = require("node-fetch");
-const { Response } = jest.requireActual("node-fetch");
-const mockSheetsData = require("./mock-sheets-data.json");
-const mockResponse = new Response(JSON.stringify(mockSheetsData));
+jest.mock("node-fetch")
+const fetch = require("node-fetch")
+const { Response } = jest.requireActual("node-fetch")
+const mockSheetsData = require("./mock-sheets-data.json")
+const mockResponse = new Response(JSON.stringify(mockSheetsData))
 
-const request = require("supertest");
-const app = require("../app");
+const request = require("supertest")
+const app = require("../app")
 
 describe("Test the root path", () => {
-  fetch.mockReturnValue(Promise.resolve(mockResponse));
-  test("It should response to the GET method", (done) => {
-    request(app)
-      .get("/")
-      .then((response) => {
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
+    fetch.mockReturnValue(Promise.resolve(mockResponse))
+    test("It should response to the GET method", done => {
+        request(app)
+            .get("/")
+            .then(response => {
+                expect(response.statusCode).toBe(200)
+                done()
+            })
+    })
+})
 
 describe("Test the stats path", () => {
-  fetch.mockReturnValue(Promise.resolve(mockResponse));
-  test("It should response to the GET method", (done) => {
-    request(app)
-      .get("/stats")
-      .then((response) => {
-        expect(response.type).toEqual("application/json");
-        expect(response.body).toHaveProperty("players");
-        expect(response.statusCode).toBe(200);
-        done();
-      });
-  });
-});
+    fetch.mockReturnValue(Promise.resolve(mockResponse))
+    test("It should response to the GET method", done => {
+        request(app)
+            .get("/stats/current")
+            .then(response => {
+                expect(response.type).toEqual("application/json")
+                expect(response.body).toHaveProperty("players")
+                expect(response.statusCode).toBe(200)
+                done()
+            })
+    })
+})
