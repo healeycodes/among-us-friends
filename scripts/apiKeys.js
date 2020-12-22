7cc07634856d807158f487e12d203499266c5c55
Andrew <healeycodes@gmail.com>
2020-12-22 02:56:12 +0000

    Move API keys

--- scripts/apiKeys.js
+++ scripts/apiKeys.js
@@ -2,7 +2,7 @@ const fs = require("fs")
 
 // Our Netlify functions need access to env vars
 fs.writeFileSync(
-    "./.env.json",
+    "../src/.env.json",
     `{
     "SHEETS_ID": "${process.env.SHEETS_ID}",
     "SHEETS_API_KEY": "${process.env.SHEETS_API_KEY}"
