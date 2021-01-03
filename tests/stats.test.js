634556178dfc80608ed4ac3fe860d11b664d410e
JEROME Parramore <jerome.parramore@gmail.com>
2021-01-03 20:01:40 +0000

    add mock data to test decay

--- tests/stats.test.js
+++ tests/stats.test.js
@@ -23,23 +23,22 @@ describe("Test buildStats", () => {
         const firstPlayer = stats.players[0]
         expect(firstPlayer.crewLoss).toStrictEqual(0)
         expect(firstPlayer.crewWin).toStrictEqual(1)
-        expect(firstPlayer.elo).toStrictEqual(1335)
+        expect(firstPlayer.elo).toStrictEqual(1337)
         expect(typeof firstPlayer.eloHistory.length).toStrictEqual("number")
-        expect(firstPlayer.games.length).toStrictEqual(43)
+        expect(firstPlayer.games.length).toStrictEqual(46)
         expect(firstPlayer.imposterLoss).toStrictEqual(0)
-        expect(firstPlayer.imposterWin).toStrictEqual(42)
+        expect(firstPlayer.imposterWin).toStrictEqual(45)
         expect(firstPlayer.name).toStrictEqual("nads")
 
-        const firstGame = firstPlayer.games[0]
+        const firstGame = firstPlayer.games[firstPlayer.games.length - 1]
 
-        // TODO: why are these broken?
-        // expect(firstGame.diff).toStrictEqual(1)
-        // expect(stats.players[9].games[1].diff).toStrictEqual(-1)
+        expect(firstGame.diff).toStrictEqual(20)
+        expect(stats.players[9].games[stats.players[9].games.length - 1].diff).toStrictEqual(-20)
 
         expect(firstGame.crew.length).toStrictEqual(8)
         expect(firstGame.crew[0]).toStrictEqual("andy")
         expect(firstGame.imposters.length).toStrictEqual(2)
-        expect(firstGame.imposters[0]).toStrictEqual("gem")
+        expect(firstGame.imposters[0]).toStrictEqual("ally")
         done()
     })
 
@@ -50,7 +49,7 @@ describe("Test buildStats", () => {
         const firstPlayer = stats.players[0]
         const eloHistory = firstPlayer.eloHistory
 
-        expect(eloHistory.length).toBe(44)
+        expect(eloHistory.length).toBe(47)
         eloHistory.forEach(elo => expect(typeof elo).toBe("number"))
         done()
     })
@@ -63,11 +62,10 @@ describe("Test buildStats", () => {
         const inactivePlayer = stats.players.find(p => p.name === "neil")
         const eloHistory = inactivePlayer.eloHistory
 
-        console.log(eloHistory) // TODO: remove
         // Their Elo should decay twice
         expect(eloHistory.length).toBe(4)
-        expect(eloHistory[1] < eloHistory[2]).toBe(true)
-        expect(eloHistory[2] < eloHistory[3]).toBe(true)
+        expect(eloHistory[1] > eloHistory[2]).toBe(true)
+        expect(eloHistory[2] > eloHistory[3]).toBe(true)
         done()
     })
 })
