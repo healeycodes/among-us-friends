3d8054f1285c69183108308d9d8610d7baf0796f
Andrew <healeycodes@gmail.com>
2021-01-13 20:55:17 +0000

    Stop map page errors

--- front-end/src/pages/Stats.js
+++ front-end/src/pages/Stats.js
@@ -15,24 +15,26 @@ export default function Stats(props) {
                     Deadliest impostor duo is <i>{deadlyDuos}</i>.
                 </p>
             )}
-            <p>
-                <b>Maps</b>
-                <br />
-                {Object.keys(season.mapData).map(name => {
-                    const crewWin = season.mapData[name].crewWin
-                    const crewLoss = season.mapData[name].crewLoss
-                    const percent = parseFloat(
-                        (crewWin / (crewWin + crewLoss)) * 100
-                    ).toFixed(2)
+            {Object.keys(season.mapData).length > 0 &&
+                <p>
+                    <b>Maps</b>
+                    <br />
+                    {Object.keys(season.mapData).map(name => {
+                        const crewWin = season.mapData[name].crewWin
+                        const crewLoss = season.mapData[name].crewLoss
+                        const percent = parseFloat(
+                            (crewWin / (crewWin + crewLoss)) * 100
+                        ).toFixed(2)
 
-                    return (
-                        <>
-                            Crew win {percent}% of the time on {getMap(name)}.
-                            <br />
-                        </>
-                    )
-                })}
-            </p>
+                        return (
+                            <>
+                                Crew win {percent}% of the time on {getMap(name)}.
+                        <br />
+                            </>
+                        )
+                    })}
+                </p>
+            }
         </div>
     )
 }
